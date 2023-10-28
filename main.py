a20b11c9bb145dbeacfadbd2bdcd4d90ee6cdb49
Francisco Castillo <castillocerna@gmail.com>
2023-10-27 19:11:33 -0600

    Triangle Textures Implemented

--- main.py
+++ main.py
@@ -6,56 +6,74 @@ from Renderer import Renderer
 from Model import Model
 from Shaders import *
 
-width = 960
-height = 540
+def main():
+    width = 960
+    height = 540
 
-pygame.init()
-screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
-clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+    clock = pygame.time.Clock()
 
-renderer = Renderer(screen)
-renderer.setShader(vertex_shader, fragment_shader)
-# x, y, z, r, g, b
-triangleData = [
-    -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-    0.0, 0.5, 0.0, 0.0, 1.0, 0.0,
-    0.5, -0.5, 0.0, 0.0, 0.0, 1.0,
-]
-triangleModel = Model(triangleData)
-triangleModel.position.z = -5
-triangleModel.scale = glm.vec3(3, 3, 3)
-renderer.scene.append(triangleModel)
+    renderer = Renderer(screen)
+    renderer.setShader(vertex_shader, fragment_shader)
+    # x, y, z, U, V
+    triangleData = [
+        -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+        -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+        0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
 
-isRunning = True
-while isRunning:
-    deltaTime = clock.tick(60) / 1000.0
-    renderer.elapsedTime += deltaTime
-    keys = pygame.key.get_pressed()
+        -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+        0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0,
+        0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+    ]
+    triangleModel = Model(triangleData)
+    triangleModel.loadTexture("Textures/text.jpg")
+    triangleModel.position.z = -5
+    triangleModel.scale = glm.vec3(3, 3, 3)
+    renderer.scene.append(triangleModel)
 
-    if keys[K_RIGHT]:
-        renderer.clearColor[0] += deltaTime
-    if keys[K_LEFT]:
-        renderer.clearColor[0] -= deltaTime
-    if keys[K_UP]:
-        renderer.clearColor[1] += deltaTime
-    if keys[K_DOWN]:
-        renderer.clearColor[1] -= deltaTime
-    if keys[K_SPACE]:
-        renderer.clearColor[2] += deltaTime
-    if keys[K_LSHIFT]:
-        renderer.clearColor[2] -= deltaTime
+    isRunning = True
+    while isRunning:
+        deltaTime = clock.tick(60) / 1000.0
+        renderer.elapsedTime += deltaTime
+        keys = pygame.key.get_pressed()
 
-    triangleModel.rotation.y += deltaTime * 50
+        if keys[K_RIGHT]:
+            renderer.clearColor[0] += deltaTime
+        if keys[K_LEFT]:
+            renderer.clearColor[0] -= deltaTime
+        if keys[K_UP]:
+            renderer.clearColor[1] += deltaTime
+        if keys[K_DOWN]:
+            renderer.clearColor[1] -= deltaTime
+        if keys[K_SPACE]:
+            renderer.clearColor[2] += deltaTime
+        if keys[K_LSHIFT]:
+            renderer.clearColor[2] -= deltaTime
 
-    # Handle quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            isRunning = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        if keys[K_d]:
+            triangleModel.rotation.y += deltaTime * 50
+        if keys[K_a]:
+            triangleModel.rotation.y -= deltaTime * 50
+        if keys[K_w]:
+            triangleModel.rotation.x += deltaTime * 50
+        if keys[K_s]:
+            triangleModel.rotation.x -= deltaTime * 50
+
+
+        # Handle quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 isRunning = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    isRunning = False
 
-    renderer.render()
-    pygame.display.flip()
+        renderer.render()
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
